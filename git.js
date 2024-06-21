d813eb5f8ab110b9bab33cc79a364d5a34def9ea
Sharan Poojari <sharanshivrampoojarissp18@gmail.com>
2024-06-21 12:15:25 +0530

    first-commit

---
+++ git.js
@@ -0,0 +1,23 @@
+// do bande mil ke banana !
+// 2 people startup 
+// remote areas mai (( us  and india))
+
+// git is used for collabrative coding (( mutliple people working on project !))
+// social media techno --> instagram 
+
+// git init --> Reinitialized existing Git repository in C:/Users/shara/OneDrive/Desktop/Github/.git/
+
+// u ( untracked ! ) --> files ke age 
+
+// account bana diya ! 
+
+// account open kiya ! 
+
+// repo --> folder !
+
+// commit --> save  (( cool ))
+
+// all the steps follow karna hai !!
+
+// git add README.md means --> sare folder( files inside) ko readyyy karna hai github pe dalne ke liye ! 
+
