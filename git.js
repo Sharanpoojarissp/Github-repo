89874dc244f9b1e4eb05b34cf12db33d9d5b0a3a
Sharan Poojari <sharanshivrampoojarissp18@gmail.com>
2024-06-21 12:30:21 +0530

    fonts and images added

--- git.js
+++ git.js
@@ -19,5 +19,39 @@
 
 // all the steps follow karna hai !!
 
-// git add README.md means --> sare folder( files inside) ko readyyy karna hai github pe dalne ke liye ! 
+// git add .    means --> sare folder( files inside) ko readyyy karna hai github pe dalne ke liye ! 
+// now all the files are having (( A )) means added symbol ! 
 
+// git commit -m "first commit"
+// Steps ! 
+// head of company (harsh)--> masterbranch control
+// others will --> other cr8 branches (( small ) / each one does their own branch work ! )
+// will push thier changes in theier own branch ! 
+// but at the end main ( harsh ) does the push work ! 
+
+// fontend / backend / db (( work ))
+
+// A is gone too! 
+
+// now !1 
+// git branch -M main --> main naam ka branch ! 
+// git remote add origin https://github.com/Sharanpoojarissp/Github-repo.git
+
+// 2 chix (( push / pull ))
+// pull : code )) means merko vo main branch se kuch chize lena hai apne laptop mai! 
+// push : code bhejna 
+
+
+// git push -u origin main --> done ! 
+
+
+//  now all the files are there in the repo !
+
+// ttf to woff2 converted ! --> as ye font exe bhari hai 
+// jpeg bhi converted to webp! --> good quality mai karna hai ! 
+
+// now woff2 jo idhar hai --> uske age U 
+// buttermilk.ttf --> deleted ! 
+// source control mai dikh gaya deleted ! 
+
+// deleted chiz ko repo mai nhi dikh rha hai (( woff2 / webp ko push karna hai ))
